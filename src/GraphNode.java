9058aec0c27859a53b2a4befbc5e9b7230eee31c
ikartiks <kartik.isworking@gmail.com>
2017-04-22 22:28:16 +0530

    modified graphnode

--- src/GraphNode.java
+++ src/GraphNode.java
@@ -1,225 +1,106 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
-class node
-{
-	node next;
-	int data;
-}
-public class GraphNode
-{
-	node first;
-	void insert(int val)
-	{
-		node x=new node();
-		x.data=val; 
-		if (first==null)
-			first=x;
-			else
-			{
-				node temp=first;
-				while(temp.next!=null)
-					temp=temp.next;
-					temp.next=x;
-			}
-	}
-	void display()
-	{
-		node temp=first;
-		while(temp!=null)//note
-		{
-			System.out.println(temp.data);
-			temp=temp.next;
-		}
-	}
-	void count()
-	{
-		int count=0;
-		node temp=first;
-		while(temp!=null)//note
-		{
-			temp=temp.next;
-			count++;
-		}
-		System.out.println("count is "+count);
-	}
-	boolean search(int v)
-	{
-		boolean flag =false;
-		node temp=first;
-		while(temp!=null)//note
-		{
-			if(v==temp.data)
-			return true;
-				else
-			           temp=temp.next;
-		}
-			return false;//note dont write conditions now
-	}
-	void insertafter (int val ,int after) throws Exception
-	{
-		boolean assume=true;
-		node prev=null;//note no need
-		node x=new node();
-		x.data=val;
-		node temp=first;
-		while(temp!=null)//note not.next it will insert to last pos even if after doesnt exists
-		{
-			if(after==temp.data)
-			{
-				x.next=temp.next;
-				temp.next=x;
-				assume =false;
-			}
-			temp=temp.next;
-		}
-		if(assume)
-			throw new Exception("value not found");
-	}
-	void insertatpos(int val,int pos)//mistake
-	{
-		node temp=first;
-		int count=1;
-		node x=new node();
-		x.data=val;
-		while (count<(pos-1))//note********************************************
-		{
-			temp=temp.next;
-			count=count+1;
-		}
-     	x.next=temp.next;
-		temp.next=x;
-			
-	}
-	void insertinorder(int val)
-	{
-		node prev=null;//note se excecution of first last and middle(check conditions (they r not same))
-		node x=new node();
-		x.data=val;
-		node temp=first;
-		while(temp!=null)
-		{
-			if(temp.data>val)
-				break;
-				else
-				{
-					prev=temp;
-					temp=temp.next;
-				}
-					
-		}
-		if(temp==first)
-		{
-			x.next=temp;
-			first=x;
-		}
-		else
-		{
-			x.next=prev.next;
-			prev.next=x;
-		}
-	}
-	GraphNode copy()//note
-	{
-		GraphNode sa=new GraphNode();
-		node temp=first;//by default l ka first , it is parameter
-		while(temp!=null)
-		{
-			sa.insert(temp.data);
-			temp=temp.next;
-		}
-		return sa;
-	}
-	GraphNode intersection(GraphNode t)
-	{
-		GraphNode l=new GraphNode();
-		node temp1=first;
-		node temp2=t.first;
-		while(temp1!=null)//since we want all elements
-		{
-			while(temp2!=null)
-			{
-				if (temp1.data==temp2.data)
-					l.insert(temp2.data);
-					temp2=temp2.next;
-			}
-			temp1=temp1.next;
-			temp2=t.first;
-		}
-		return l;
-	}
-	GraphNode union(GraphNode t)
-	{
-		GraphNode l=new GraphNode();
-		node temp1=first;
-		node temp2=t.first;
-		while(temp1.next!=null)
-			{
-				l.insert(temp1.data);
-				temp1=temp1.next;
-			}
-		while(temp1!=null)//since we want all elements
-		{
-			while(temp2!=null)
-			{
-				if (temp1.data==temp2.data)
-				{temp2=temp2.next;}
-				else
-				{
-					l.insert(temp2.data);
-					temp2=temp2.next;	
-				}
-					
-			}
-			temp1=temp1.next;
-			temp2=t.first;
-		}
-		return l;
+public class GraphNode {
+	
+	List<GraphNode> toNodes;
+	static List<String> visitedNodes=new ArrayList<String>();
+	String data;
+
+	public static void main(String[] args) {
+		GraphNode one=new GraphNode("One");
+		GraphNode two=new GraphNode("two");
+		GraphNode three =new GraphNode("three");
+		GraphNode four =new GraphNode("four");
+		one.setToNodes(Arrays.asList(two,three));
+		two.setToNodes(Arrays.asList(one));
+		three.setToNodes(Arrays.asList(one,two,four));
+		
+		visitedNodes.clear();
+		System.out.println(pathExists(one, four));
+		visitedNodes.clear();
+		System.out.println(pathExists(four, one));
 	}
 	
-	public static boolean findPath(node start, node end){
+	
+	
+	public GraphNode(String data) {
+		super();
+		this.data = data;
+	}
+	
+	@Override
+	public String toString() {
 		
-		node startNode=start;
-		while (start.next!=null) {
-			if(start.next.data==end.data){
-				return true;
-			}
-			start=start.next;
-			
+		StringBuilder sb=new StringBuilder();
+		sb.append(data+" - ");
+		for (Iterator iterator = toNodes.iterator(); iterator.hasNext();) {
+			GraphNode graphNode = (GraphNode) iterator.next();
+			sb.append(graphNode.getData()+", ");
 		}
+		return sb.toString();
+	}
+
+
+
+	public void findPath(GraphNode from, GraphNode to){
+		
+	}
+	
+	public static boolean pathExists(GraphNode from, GraphNode to){
+		
+		
+		if(visitedNodes.contains(from.getData())){
+			//System.out.println("node already visited "+from.getData());
+			return false;
+		}
+		
+		List<GraphNode> nodes=from.getToNodes();//one - 2,3
+		if(nodes==null){
+			visitedNodes.add(from.getData());
+			return false;
+		}
+		
+		visitedNodes.add(from.getData());
+		for (Iterator iterator = nodes.iterator(); iterator.hasNext();) {
+			GraphNode graphNode = (GraphNode) iterator.next();
+			if(graphNode.getData().equals(to.getData())){
+				return true;
+			}else if(graphNode.getToNodes()!=null ){
+				
+				if( (!visitedNodes.contains(graphNode.getData())) && (!graphNode.getData().equals(from.getData()))){
+					
+					//System.out.println("from "+from.getData() +" child "+graphNode.getData());
+					boolean op=pathExists(graphNode, to);
+					if(op)
+						return true;
+					else
+						continue;
+				}
+			}
+		}
+		
+		//System.out.println("visited "+from.getData());
 		return false;
 	}
 	
-	public static void  main(String arg[])
-	{
-		GraphNode l=new GraphNode();
-		l.insert(25);
-		l.insert(35);
-		l.insert(25);
-		l.insert(55);
-		//l.display();
-		l.count();//note
-		System.out.println(l.search(15));
-		try{
-			l.insertafter(11,2566);
-		}
-		catch(Exception e){System.out.println(e);}
-		try{l.insertatpos(87,3);
-			}
-			catch(Exception e){System.out.println(e);}
-		l.display();
-		/*
-		l.insertinorder(20);
-		l.insertinorder(30);
-		l.insertinorder(99);
-		ll s=l.copy();
-		ll b=new ll();
-		b.insert(20);
-		b.insert(77);
-		b.insert(111);
-		b.insert(35);
-		ll a=s.intersection(b);
-		ll d=l.union(b);//mistake
-		d.display();
-		*/
+	public List<GraphNode> getToNodes() {
+		return toNodes;
 	}
+
+	public void setToNodes(List<GraphNode> toNodes) {
+		this.toNodes = toNodes;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
 }
